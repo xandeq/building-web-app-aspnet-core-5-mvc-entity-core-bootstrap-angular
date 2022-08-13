52bcf29965d166df20c1caada755b07424258a7f
xandeq@gmail.comm <xandeq@gmail.comm>
2022-08-12 23:50:21 -0300

    And Bootstrap

---
+++ Data/DutchRepository.cs
@@ -0,0 +1,48 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DutchTreat.Data
+{
+    public class DutchRepository : IDutchRepository
+    {
+        private readonly DutchContext _ctx;
+        private readonly ILogger<DutchRepository> _logger;
+
+        public DutchRepository(DutchContext ctx, ILogger<DutchRepository> logger)
+        {
+            _ctx = ctx;
+            _logger = logger;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            try
+            {
+                _logger.LogInformation("Get All Products was called.");
+                return _ctx.Products
+                    .OrderBy(p => p.Title)
+                    .ToList();
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError($"Error. {ex}");
+                throw;
+            }
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return _ctx.Products
+                .Where(p => p.Category == category)
+                .OrderBy(p => p.Title)
+                .ToList();
+        }
+
+        public bool SaveAll()
+        {
+            return _ctx.SaveChanges() > 0;
+        }
+    }
+}
